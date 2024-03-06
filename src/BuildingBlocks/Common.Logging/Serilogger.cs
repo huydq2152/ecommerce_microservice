60938c797363421e479aa79fc999c5ac500b75bb
huydq2152 <quanghuy2152@gmail.com>
2024-03-06 21:18:19 +0700

    add Serilog to Elasticsearch and Kibana

--- src/BuildingBlocks/Common.Logging/Serilogger.cs
+++ src/BuildingBlocks/Common.Logging/Serilogger.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace Common.Logging;
 
@@ -10,11 +12,22 @@ public static class Serilogger
         {
             var applicationName = context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-");
             var environmentName = context.HostingEnvironment.EnvironmentName ?? "Development";
+            var elasticUri = context.Configuration.GetValue<string>("ElasticConfiguration:Uri");
+            var userName = context.Configuration.GetValue<string>("ElasticConfiguration:UserName");
+            var password = context.Configuration.GetValue<string>("ElasticConfiguration:Password");
 
             configuration
                 .WriteTo.Debug()
                 .WriteTo.Console(outputTemplate:
                     "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri))
+                {
+                    IndexFormat = $"microservice-{applicationName}-{environmentName}-{DateTime.UtcNow:yyyy-MM}",
+                    AutoRegisterTemplate = true,
+                    NumberOfShards = 2,
+                    NumberOfReplicas = 1,
+                    ModifyConnectionSettings = c=>c.BasicAuthentication(userName, password)
+                })
                 .Enrich.FromLogContext()
                 .Enrich.WithMachineName()
                 .Enrich.WithProperty("Environment", environmentName)
