a392bdeec723aea7ee20e71335cd7cc7722d8a1e
huydq2152 <quanghuy2152@gmail.com>
2024-03-08 23:31:07 +0700

    add identity server and fix bug login

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,25 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            { };
+
+    public static IEnumerable<ApiResource> ApiResources =>
+        new ApiResource[]
+            { };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+            { };
+}
---
+++ src/IdentityServer/Program.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServer;
+using IdentityServer.Extensions;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    var app = builder
+        .ConfigureServices()
+        .ConfigurePipeline();
+    
+    app.Run();
+}
+catch (Exception ex)
+{
+    // block ex when running migrations
+    var type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal))
+    {
+        throw;
+    }
+
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shutdown identity server success");
+    Log.CloseAndFlush();
+}
