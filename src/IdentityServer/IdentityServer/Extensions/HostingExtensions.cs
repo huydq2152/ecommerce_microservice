375f1e523ad2421ad111891aae1da06d1a6c1a76
huydq2152 <quanghuy2152@gmail.com>
2024-04-09 20:43:57 +0700

    update Identity project

---
+++ src/IdentityServer/IdentityServer/Extensions/HostingExtensions.cs
@@ -0,0 +1,95 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+namespace IdentityServer.Extensions;
+
+internal static class HostingExtensions
+{
+    internal static void AddAppConfigurations(this ConfigureHostBuilder host)
+    {
+        host.ConfigureAppConfiguration((context, config) =>
+        {
+            var env = context.HostingEnvironment;
+            config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+        });
+    }
+    
+    public static void ConfigureSerilog(this ConfigureHostBuilder host)
+    {
+        host.UseSerilog((context, configuration) =>
+        {
+            var elasticUri = context.Configuration.GetValue<string>("ElasticConfiguration:Uri");
+            var username = context.Configuration.GetValue<string>("ElasticConfiguration:Username");
+            var password = context.Configuration.GetValue<string>("ElasticConfiguration:Password");
+            var applicationName = context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-");
+
+            if (string.IsNullOrEmpty(elasticUri))
+                throw new Exception("ElasticConfiguration Uri is not configured.");
+
+            configuration
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .WriteTo.Debug()
+                .WriteTo.Console().ReadFrom.Configuration(context.Configuration)
+                .WriteTo.Elasticsearch(
+                    new ElasticsearchSinkOptions(new Uri(elasticUri))
+                    {
+                        IndexFormat =
+                            $"{applicationName}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                        AutoRegisterTemplate = true,
+                        NumberOfShards = 2,
+                        NumberOfReplicas = 1,
+                        ModifyConnectionSettings = x => x.BasicAuthentication(username, password)
+                    })
+                .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                .Enrich.WithProperty("Application", applicationName)
+                .ReadFrom.Configuration(context.Configuration);
+        });
+    }
+    
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        // uncomment if you want to add a UI
+        builder.Services.AddRazorPages();
+
+        builder.Services.ConfigureIdentityServer(builder.Configuration);
+        builder.Services.ConfigureCookiePolicy();
+        
+        builder.Services.ConfigureCors();
+        
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        app.UseSerilogRequestLogging();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        // uncomment if you want to add a UI
+        app.UseStaticFiles();
+        
+        app.UseCors("CorsPolicy");
+        app.UseRouting();
+
+        app.UseCookiePolicy();
+        app.UseIdentityServer();
+
+        // uncomment if you want to add a UI
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapDefaultControllerRoute().RequireAuthorization();
+            endpoints.MapRazorPages().RequireAuthorization();
+        });
+
+        return app;
+    }
+    
+    
+}
---
+++ src/IdentityServer/IdentityServer/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using Duende.IdentityServer;
+using System.Reflection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServer.Pages.Home;
+
+[AllowAnonymous]
+public class Index : PageModel
+{
+    public Index()
+    {
+    }
+
+    public string Version
+    {
+        get => typeof(Duende.IdentityServer.Hosting.IdentityServerMiddleware).Assembly
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+            ?.InformationalVersion.Split('+').First()
+            ?? "unavailable";
+    }
+}
